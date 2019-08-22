dcdb56f5bb90b3418fecbf94a788826bfaff31a3
Hayden Bailey <hayden.bailey@my.jcu.edu.au>
2019-08-22 11:30:20 +1000

    Amend minimum password length

--- password_entry.py
+++ password_entry.py
@@ -2,8 +2,8 @@
 Hayden Bailey
 """
 
-password = input('Input password (min 6 characters): ')
-while len(password) <= 5:
+password = input('Input password (min 7 characters): ')
+while len(password) <= 6:
     password = input('Incorrect length. Input password: ')
 
 for char in range(len(password)):
